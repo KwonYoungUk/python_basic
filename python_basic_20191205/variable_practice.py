08911415355c48c8b6ff10d0adb85c5539d439bd
Kwon <younguk5213@gmail.com>
2019-12-05 23:15:04 +0900

    20191205 Variable

---
+++ python_basic_20191205/variable_practice.py
@@ -0,0 +1,34 @@
+# # 변수
+# # 1.String
+# myString = 'Hello World'
+# print(myString)
+#
+# # 2.Numeric (int, float)
+# myInt = 3;
+# myFloat = 3.14;
+# print(myInt, myFloat)
+#
+# # 3.List, Tuple, Dictionary
+# myList = [1, 2, 3, 4];
+# myTuple = ('권영욱', '전혜리', '김건우');
+# myDictionary = {'name' : 'Kwon', 'Age' : 31, 'job' : 'Programmer'};
+# print(myList);4
+# print(myTuple);
+# print(myDictionary);
+#
+# # print(), input(), print('', end = '')
+#  문자열 포맷 출력
+# # type1
+# # %s -> 문자열 / %d -> Nubmer형
+# print('My Name is %s and my Age is %d' % ('Kwon', 31))
+# # type2
+# # 타입에 상관없이 {}로 처리
+# print('My name is {} and my Age is {}'.format('Kwon', 31))
+# print('집단지성', end=' This is End')
+# print("{1} X {0} = {2} 0".format(2, 3, 6))
+#
+# print('미운', end='\t');
+# # 입력값 출력
+# inputString = input('나이를 입력해주세요 : ')
+# print('Your Age is %d' % int(inputString))
+#
